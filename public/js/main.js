985faeebc09d549fd2f8f0fc5c7bc8c5cba7406b
Karlo Mikus <karlo.mikus1@gmail.com>
2015-05-04 00:09:02 +0200

    Updated javascript functions

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,15 @@
 
 var app = angular.module('cometapp', []);
 
-app.controller('MatchesController', function($scope) {
-    
+app.controller('MatchesController', function($scope, MatchFormData) {
+    MatchFormData.get().then(function(response) {
+        $scope.form = response.data;
+    });
+});
+
+app.service('MatchFormData', function($http) {
+    this.get = function() {
+        return $http.get('/admin/matches/form');
+    }
 });
 //# sourceMappingURL=main.js.map
