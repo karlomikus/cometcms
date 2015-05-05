93bab027a8c10faae208647738f8fdbcedb8e704
Karlo Mikus <karlo.mikus1@gmail.com>
2015-05-05 19:47:00 +0200

    Added user roles and permissions migrations + seeding

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 /**
  * App\User
@@ -26,7 +27,7 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
  */
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
-	use Authenticatable, CanResetPassword;
+	use Authenticatable, CanResetPassword, EntrustUserTrait;
 
 	/**
 	 * The database table used by the model.
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
+use App\User, App\Role;
 
 class UsersTableSeeder extends Seeder {
 
@@ -9,18 +9,30 @@ class UsersTableSeeder extends Seeder {
     {
         $faker = Faker\Factory::create();
 
+        $roleUsers = new Role();
+        $roleUsers->name = 'user';
+        $roleUsers->display_name = 'Users';
+        $roleUsers->description = 'All registered users';
+        $roleUsers->save();
+
+        $roleAdmins = new Role();
+        $roleAdmins->name = 'admin';
+        $roleAdmins->display_name = 'Admins';
+        $roleAdmins->description = 'All site administrators';
+        $roleAdmins->save();
+
         User::create([
             'name' => 'Karlo',
             'email' => 'admin@admin.com',
             'password' => Hash::make('admin123')
-        ]);
+        ])->attachRole($roleAdmins);
 
         for ($i=0; $i < 10; $i++) {
             User::create([
                 'name' => $faker->name,
                 'email' => $faker->email,
                 'password' => Hash::make('demo123')
-            ]);
+            ])->attachRole($roleUsers);
         }
     }
 
