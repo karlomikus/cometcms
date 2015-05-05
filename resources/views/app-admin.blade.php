a9466ed668ec406dbd4f91fe3299030f51cb531d
Karlo Mikus <karlo.mikus1@gmail.com>
2015-05-05 20:04:29 +0200

    Updated app layouts

---
+++ resources/views/app-admin.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="cometapp">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CometCMS App</title>
+
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/admin') }}">CometCMS Administration</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="{{ url('/admin/matches') }}">Matches</a></li>
+                <li><a href="{{ url('/admin/matches/new') }}">New match</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{ url('/') }}">View site</a></li>
+                        <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+@yield('content')
+
+<!-- Scripts -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script src="{{ asset('/js/dependencies.js') }}"></script>
+<script src="{{ asset('/js/main.js') }}"></script>
+</body>
+</html>
--- resources/views/matches/form.blade.php
+++ resources/views/matches/form.blade.php
@@ -1,4 +1,4 @@
-@extends('app')
+@extends('app-admin')
 
 @section('content')
 
