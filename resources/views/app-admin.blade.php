9369b7c5a48f1d77cb9faae43c96402310979668
Karlo Mikus <karlo.mikus1@gmail.com>
2015-05-12 19:49:45 +0200

    Updated admin navigation

--- resources/views/app-admin.blade.php
+++ resources/views/app-admin.blade.php
@@ -31,14 +31,36 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="{{ url('/admin/matches') }}">Matches</a></li>
-                <li><a href="{{ url('/admin/users') }}">Users</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Content <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{ url('/admin/matches') }}">TODO</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Team management <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{ url('/admin/matches') }}">Matches</a></li>
+                        <li><a href="{{ url('/admin/opponents') }}">Opponents</a></li>
+                        <li><a href="{{ url('/admin/teams') }}">Teams</a></li>
+                        <li><a href="{{ url('/admin/maps') }}">Maps</a></li>
+                        <li><a href="{{ url('/admin/games') }}">Games</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Users management <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{ url('/admin/users') }}">Users</a></li>
+                        <li><a href="{{ url('/admin/roles') }}">Roles</a></li>
+                        <li><a href="{{ url('/admin/permissions') }}">Permissions</a></li>
+                    </ul>
+                </li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }} <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
                         <li><a href="{{ url('/') }}">View site</a></li>
                         <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
                     </ul>
