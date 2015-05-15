4df5b72109a9653325893982ff048653388794c6
Karlo Mikus <karlo.mikus1@gmail.com>
2015-05-15 19:56:38 +0200

    Added error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Page missing or under construction! <small>error: 404</small></h1>
+            </div>
+        </div>
+    </div>
+
+@endsection
