9c9d672d7446578a059b9add346bf81f27c2b5ad
karlomikus <karlo.mikus1@gmail.com>
2015-07-20 08:44:58 +0000

    Updated roles CRUD

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -4,6 +4,7 @@ use App\Libraries\GridView\GridView;
 use App\Repositories\Contracts\RolesRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use App\Http\Requests\SaveRoleRequest;
 
 class RolesController extends AdminController {
 
@@ -47,7 +48,7 @@ class RolesController extends AdminController {
         return view('admin.roles.form', $template);
     }
 
-    public function save(Request $request)
+    public function save(SaveRoleRequest $request)
     {
         $displayName = $request->input('display_name');
         $name = Str::slug($displayName);
@@ -61,7 +62,9 @@ class RolesController extends AdminController {
         $perms = $request->input('perms');
 
         if ($role) {
-            $role->attachPermissions($perms);
+            if (!empty($perms)) {
+                $role->attachPermissions($perms);
+            }
             $this->alerts->alertSuccess('New role created successfully!');
         }
         else {
@@ -86,7 +89,7 @@ class RolesController extends AdminController {
         return view('admin.roles.form', $template);
     }
 
-    public function update($id, Request $request)
+    public function update($id, SaveRoleRequest $request)
     {
         $displayName = $request->input('display_name');
         $name = Str::slug($displayName);
@@ -97,11 +100,32 @@ class RolesController extends AdminController {
             'description' => $request->input('description')
         ]);
 
+        $perms = $request->input('perms');
+
         if ($role) {
+            if (!empty($perms)) {
+                // TODO: Fix perms editing
+                //$role->detachPermission([]);
+                $role->attachPermissions($perms);
+            }
             $this->alerts->alertSuccess('Role edited successfully!');
         }
         else {
             $this->alerts->alertError('Role edit failed!');
+        }
+
+        $this->alerts->getAlerts();
+
+        return redirect('admin/roles');
+    }
+
+    public function delete($id)
+    {
+        if ($this->roles->delete($id)) {
+            $this->alerts->alertSuccess('Role deleted successfully!');
+        }
+        else {
+            $this->alerts->alertError('Unable to delete a role!');
         }
 
         $this->alerts->getAlerts();
