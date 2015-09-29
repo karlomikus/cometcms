8dcd5b61b3b0b003480e68a48094b8176ecc0171
karlomikus <karlo.mikus1@gmail.com>
2015-09-29 13:35:05 +0200

    Updated styles

--- resources/views/admin/app-admin.blade.php
+++ resources/views/admin/app-admin.blade.php
@@ -19,7 +19,7 @@
 <nav class="navbar navbar-admin navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-nav">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -28,7 +28,7 @@
             {{-- <a class="navbar-brand" href="{{ url('/admin') }}">Dashboard</a> --}}
         </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="navbar-collapse-nav">
             <ul class="nav navbar-nav">
                 <li>
                     <a href="{{ url('/admin') }}">Dashboard</a>
