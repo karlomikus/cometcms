d2d7363cfb0691ed51e88dde4964d344e6ae031f
karlomikus <contact@karlomikus.com>
2016-07-09 17:25:45 +0000

    Added common exceptions

---
+++ app/Core/Common/Exceptions/ImageUploadException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Comet\Core\Common\Exceptions;
+
+use Exception;
+
+class ImageUploadException extends Exception
+{
+
+}
---
+++ app/Core/Common/Exceptions/RepositoryException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Comet\Core\Common\Exceptions;
+
+use Exception;
+
+class RepositoryException extends Exception
+{
+
+}
